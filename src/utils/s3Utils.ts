32edfcf0baf7a8712a07517c9fe5252851c3bb39
Vibhor Nikhra <vibhor.nikhra@gmail.com>
2024-01-28 09:32:42 -0500

    Added worker logic

--- src/utils/s3Utils.ts
+++ src/utils/s3Utils.ts
@@ -1,7 +1,6 @@
 import {S3} from "aws-sdk";
-import {Readable} from "node:stream";
 export function downloadFileFromS3(s3: AWS.S3, params: S3.GetObjectRequest): Promise<any> {
-    return new Promise<Readable>((resolve) => {
+    return new Promise((resolve) => {
         const s3Stream = s3.getObject(params).createReadStream();
         resolve(s3Stream);
     });
